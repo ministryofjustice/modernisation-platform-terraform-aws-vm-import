59b399b23e80329f2220d4bd3401c1fa35fa6660
zuriguardiola <zuri.guardiola@digital.justice.gov.uk>
2022-06-16 11:22:23 +0100

    Fix issues with test and add go.mod and go.sum files

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -1,9 +1,9 @@
 output "role_name" {
-  value       = module.vm-import.role
+  value       = module.vm-import.role.name
   description = "IAM role name output"
 }
 
 output "policy_name" {
-  value       = module.vm-import.policy
+  value       = module.vm-import.policy.name
   description = "IAM policy name output"
 }
