3467e103e5c8fed2e83896ac9079c54277e60e8d
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2025-07-16 11:31:36 +0100

    fix tsa warning

--- test/unit-test/versions.tf
+++ test/unit-test/versions.tf
@@ -4,6 +4,10 @@ terraform {
       version = "~> 6.0"
       source  = "hashicorp/aws"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
   }
   required_version = "~> 1.0"
 }
