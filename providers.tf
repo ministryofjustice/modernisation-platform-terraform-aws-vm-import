791f67ea83008bc0e1b4668d7ca944ac4105a773
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2026-05-13 12:48:24 +0100

    fix: declare aws.bucket-replication as configuration alias

--- providers.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  alias  = "bucket-replication"
-  region = var.region
-  assume_role {
-    role_arn = "arn:aws:iam::${var.account_number}:role/MemberInfrastructureAccess"
-  }
-}
--- versions.tf
+++ versions.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 6.0"
-      source  = "hashicorp/aws"
+      version               = "~> 6.0"
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws.bucket-replication]
     }
   }
   required_version = "~> 1.0"
